31ceffae5a07a5e0e39273e8acacb644ac0a658f
gogan karmur <gogankarmur007@gmail.com>
2026-06-30 23:41:14 +0530

    Starting Arrays

---
+++ Java Language/6Array/Basic.java
@@ -0,0 +1,17 @@
+public class Basic {
+    public static void main(String[] args) {
+
+        int[] marks = new int[3];
+        marks[0] = 92; // Maths
+        marks[1] = 74; // Phy
+        marks[2] = 68; //Che
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]+ "\n" + marks[2]);
+
+        System.out.println("Maths --> " + marks[0]);
+        System.out.println("Phy --> " + marks[1]);
+        System.out.println("Che --> " + marks[2]);
+
+    }
+}
