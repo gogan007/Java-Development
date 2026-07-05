e9ea05c6c3fa0f96c4a64d4646c1b6156d31973f
gogan karmur <gogankarmur007@gmail.com>
2026-07-05 23:47:12 +0530

    Recursion Completed...

---
+++ 2. DSA/1Sorting/Selection.java
@@ -0,0 +1,30 @@
+public class Selection {
+
+    //Function for Print Array
+    public static void printArray(int arr[]) { 
+       for(int i=0; i<arr.length; i++) { 
+           System.out.print(arr[i]+" "); 
+       } 
+       System.out.println(); 
+   } 
+ 
+   public static void main(String args[]) { 
+       int arr[] = {7, 8, 1, 3, 2}; 
+ 
+       //selection sort 
+       for(int i=0; i<arr.length-1; i++) { 
+           int min = i;
+           for(int j=i+1; j<arr.length; j++) { 
+               if(arr[j] < arr[min]) { 
+                   min = j; 
+               } 
+           } 
+           //swap 
+           int temp = arr[min]; 
+           arr[min] = arr[i]; 
+           arr[i] = temp; 
+       } 
+ 
+       printArray(arr); 
+   }
+}
