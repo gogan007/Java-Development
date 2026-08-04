087a080464d576d06c5f243e29388aa832e4f45f
gogan karmur <gogankarmur007@gmail.com>
2026-08-04 22:59:59 +0530

    Stack using Array List..

---
+++ 2. DSA/3Stack/ArrayList/Basic.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Basic {
+
+    class Stack {
+
+        ArrayList <Integer> list = new ArrayList<Integer>();
+
+        // Check empty stack
+        public boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        //Push operation in stack
+        public void push(int data) {
+            list.add(data);
+        }
+
+        // Pop operation in stack
+        public int pop() {
+
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        // Peek operation in stack
+        public int peek(){
+
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+
+    }
+    public static void main(String[] args) {
+
+        Basic obj = new Basic();
+        Stack num = obj.new Stack();
+        
+        num.push(1);
+        num.push(2);
+        num.push(3);
+        num.push(4);
+
+        while (!(num.isEmpty())) {
+            System.out.print(num.peek() + " --> ");
+            num.pop();
+        }
+        System.out.println("null");
+    }
+}
